e7f596bb87059c59f9581128fe6d931bb80ed44d
Beicker Zambrano <beicker998@gmail.com>
2021-07-29 07:05:36 -0500

    Add cors library to solve cors problems

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 const productRouter = require("./routes/product");
@@ -6,18 +7,7 @@ const categoryRouter = require("./routes/category");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH");
-  }
-  next();
-});
+app.use(cors());
 
 app.use("/product", productRouter);
 app.use("/category", categoryRouter);
