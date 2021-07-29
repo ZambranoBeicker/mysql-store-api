f69c4287e344461793d6e252677ff3623cd209e1
Beicker Zambrano <beicker998@gmail.com>
2021-07-29 06:40:59 -0500

    Fix request not ending
    Change the cors middleware to fix the above error. Change the queries
    and database connection usage to properly use the mysql pool

--- src/app.js
+++ src/app.js
@@ -7,9 +7,13 @@ const categoryRouter = require("./routes/category");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use((req, res) => {
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, Content-Type");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
 app.use("/product", productRouter);
--- src/routes/product.js
+++ src/routes/product.js
@@ -6,98 +6,107 @@ const {
   deleteData,
   updateData,
 } = require("../utils/queries");
-const dbConnection = require("../database");
+const getConnection = require("../utils/connection");
 
 router.get("/", (req, res) => {
   if (req.query.search) {
-    dbConnection.query(
-      `SELECT * FROM product WHERE name LIKE '%${req.query.search}%'`,
-      (err, rows, field) => {
-        if (err) {
-          throw err;
-        }
+    getConnection((connection) => {
+      connection.query(
+        `SELECT * FROM product WHERE name LIKE '%${req.query.search}%'`,
+        (err, rows, field) => {
+          if (err) {
+            throw err;
+          }
 
-        res.status(200).json({
-          message: "GET to products successfully",
-          data: rows,
-        });
-      }
-    );
+          res.status(200).json({
+            message: "GET to products successfully",
+            data: rows,
+          });
+        }
+      );
+    });
 
     return;
   }
 
-  dbConnection.query(getAllData("*", "product"), (err, rows, field) => {
-    res.status(200).json({
-      message: "GET to products successfully",
-      data: rows,
+  getConnection((connection) => {
+    connection.query(getAllData("*", "product"), (err, rows, field) => {
+      res.status(200).json({
+        message: "GET to products successfully",
+        data: rows,
+      });
     });
   });
 });
 
 router.post("/", (req, res) => {
-  //
   const { name, price, category, discount, url_image } = req.body;
 
-  dbConnection.query(
-    insertData(
-      "name, price, category, discount, url_image",
-      "product",
-      `?, ?, ?, ?, ?`
-    ),
-    [name, price, category, discount, url_image],
-    (err, rows, field) => {
-      if (!err) {
-        res.status(200).json({
-          message: "POST to products successfully",
-          data: rows,
-        });
-      } else {
-        throw err;
+  getConnection((connection) => {
+    connection.query(
+      insertData(
+        "name, price, category, discount, url_image",
+        "product",
+        `?, ?, ?, ?, ?`
+      ),
+      [name, price, category, discount, url_image],
+      (err, rows, field) => {
+        if (!err) {
+          res.status(200).json({
+            message: "POST to products successfully",
+            data: rows,
+          });
+        } else {
+          throw err;
+        }
       }
-    }
-  );
+    );
+  });
 });
 
 router.put("/:id", (req, res) => {
   const { name, price, category, discount, url_image } = req.body;
 
-  dbConnection.query(
-    updateData(
-      "product",
-      "name =?, price =?, discount = ?, url_image = ?, category =?",
-      "id = ?"
-    ),
+  getConnection((connection) => {
+    connection.query(
+      updateData(
+        "product",
+        "name =?, price =?, discount = ?, url_image = ?, category =?",
+        "id = ?"
+      ),
 
-    [name, price, discount, url_image, category, req.params.id],
-    (err, rows, field) => {
-      if (!err) {
-        res.status(200).json({
-          message: "PUT to products successfully",
-          data: rows,
-        });
-      } else {
-        throw err;
+      [name, price, discount, url_image, category, req.params.id],
+      (err, rows, field) => {
+        if (!err) {
+          res.status(200).json({
+            message: "PUT to products successfully",
+            data: rows,
+          });
+        } else {
+          throw err;
+        }
       }
-    }
-  );
+    );
+  });
 });
 
 router.delete("/:id", (req, res) => {
-  dbConnection.query(
-    deleteData("product", "id =?"),
-    [req.params.id],
-    (err, rows, field) => {
-      if (!err) {
-        res.status(200).json({
-          message: "DELETE to products successfully",
-          data: rows,
-        });
-      } else {
-        throw err;
+  getConnection((connection) => {
+    connection.query(
+      deleteData("product", "id =?"),
+      [req.params.id],
+      (err, rows, field) => {
+        if (!err) {
+          res.status(200).json({
+            message: "DELETE to products successfully",
+            data: rows,
+          });
+        } else {
+          throw err;
+        }
       }
-    }
-  );
+    );
+  });
 });
 
 module.exports = router;
