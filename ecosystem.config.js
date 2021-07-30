25fe9874f0655dab90b3dfd3050e9404f7612f48
Beicker Zambrano <beicker998@gmail.com>
2021-07-29 19:46:34 -0500

    Add pm2 integration with heroku

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      name: "mysql-store-api",
+      script: "./src/server.js",
+      watch: ".",
+      ignore_watch: ["[/\\]./", "node_modules"],
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: process.env.USER,
+      host: process.env.HOST,
+      ref: "origin/main",
+      path: "/app",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "npm install && pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+    },
+  },
+};
