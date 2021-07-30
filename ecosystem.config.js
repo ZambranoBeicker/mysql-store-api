135b66ec0b634f70cd0b728d1fa7a92098fd346f
Beicker Zambrano <beicker998@gmail.com>
2021-07-29 21:28:23 -0500

    Ignore unnecesary production folders

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,15 @@ module.exports = {
       name: "mysql-store-api",
       script: "./src/server.js",
       watch: ".",
-      ignore_watch: ["[/\\]./", "node_modules"],
+      ignore_watch: [
+        "[/\\]./",
+        "node_modules",
+        ".heroku",
+        ".profile.d",
+        ".pm2",
+        ".config",
+        ".npm",
+      ],
     },
   ],
 
